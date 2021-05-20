a13a6c0eb886038bc70630ec0cdbfad68fa4fc51
kial917 <kial917@mail.ru>
2021-05-20 17:30:28 +0300

    16.1 Block03 with film's list is done

--- js/films-table.js
+++ js/films-table.js
@@ -1,4 +1,6 @@
 const films = [{
+    id: 26,
+    link: 838,
     start: '10:00',
     title: 'Человек-паук',
     genre: [{
@@ -12,6 +14,8 @@ const films = [{
         },
     ]
 }, {
+    id: 27,
+    link: 1122114,
     start: '12:00',
     title: 'Собачья жизнь 2',
     genre: [{
@@ -25,6 +29,8 @@ const films = [{
         },
     ]
 }, {
+    id: 28,
+    link: 846824,
     start: '14:00',
     title: 'История игрушек 4',
     genre: [{
@@ -38,6 +44,8 @@ const films = [{
         },
     ]
 }, {
+    id: 29,
+    link: 693730,
     start: '16:00',
     title: 'Люди в чёрном: Интэрнэшнл',
     genre: [{
@@ -50,28 +58,63 @@ const films = [{
             name: 'комедия',
         },
     ]
+}, {
+    id: 30,
+    start: '01:00',
+    title: 'Кожаные барсетки 5',
+    adult: true,
+    genre: [{
+            name: 'фанстастика',
+        },
+
+    ]
 }, ];
+
+const filmHelper = {
+    getId (){
+        return this.id || this.title.replaceALL(' ','-')
+    },
+    getTitle (){
+        return this.title;
+    },
+    getStart(){
+        return this.start;
+    },
+    getGenre(){
+        return this.genre.map(g=>g.name).join(', ');
+    },
+    getLink(){
+        return this.link;
+    },
+};
+
+function renderFilmTableItem(film) {
+    return `
+        <tr>
+            <td class="block03__table__firstcol">
+                <input type="checkbox" id="${filmHelper.getId.apply(film)}" class="visuallyhidden">
+
+                <label for="${filmHelper.getId.apply(film)}">
+                    <svg width="11" height="9" viewBox="0 0 11 9" fill="white"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path class="checkMark__path" fill-rule="evenodd" clip-rule="evenodd"
+                            d="M4.60581 6.79378L1.46056 3.93033L0.787354 4.66979L4.70255 8.23421L10.8223 0.94099L10.0562 0.298203L4.60581 6.79378Z"
+                            />
+                    </svg>
+                </label>
+            </td>
+            <td>${filmHelper.getStart.apply(film)}</td>
+            <td><a href="https://www.kinopoisk.ru/film/${filmHelper.getLink.apply(film)}" target="_blank">${filmHelper.getTitle.apply(film)}</a> </td>
+            <td>${filmHelper.getGenre.apply(film)}</td>
+        </tr>
+    `
+}
 
 const tableBody = document.getElementById('block03__table__body');
 tableBody.innerHTML = ``;
 
-for (let i = 0; i < films.length; i++) {
-    tableBody.innerHTML += `
-    <tr>
-                            <td class="block03__table__firstcol">
-
-                                <svg width="11" height="9" viewBox="0 0 11 9" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path class="checkMark__path" fill-rule="evenodd" clip-rule="evenodd"
-                                        d="M4.60581 6.79378L1.46056 3.93033L0.787354 4.66979L4.70255 8.23421L10.8223 0.94099L10.0562 0.298203L4.60581 6.79378Z"
-                                        fill="white" />
-                                </svg>
-
-
-                            </td>
-                            <td>${films[i].start}</td>
-                            <td><a href="https://www.kinopoisk.ru/film/838/" target="_blank">${films[i].title}</a> </td>
-                            <td>${films[i].genre.map(g=>g.name).join(', ')}</td>
-                        </tr>
-    `
-};
+films.forEach(film=>{
+    if (!film.adult) {
+        tableBody.innerHTML += renderFilmTableItem(film);
+    }
+})
