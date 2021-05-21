795207a562e32aa9410534254212d94a329f5c9c
kial917 <kial917@mail.ru>
2021-05-21 17:35:49 +0300

    16.3 Gift block has finished without content

--- js/gift.js
+++ js/gift.js
@@ -1,8 +1,10 @@
 const openBtn = document.getElementById('btn-gift-open');
 const popup = document.getElementById('gift-popup');
+const closeBtn =document.getElementById('gift-popup-close');
 
 function popupToggle(){
     popup.classList.toggle('hidden');
 };
 
-openBtn.onclick = popupToggle();
+openBtn.onclick = popupToggle;
+closeBtn.onclick = popupToggle;
