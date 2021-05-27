3529c6d6520377785f566fb72068310983e87e2c
kial917 <kial917@mail.ru>
2021-05-27 03:12:34 +0300

    17.6 fetch method has formed in modal window

--- js/gift.js
+++ js/gift.js
@@ -1,12 +1,61 @@
 const openBtn = document.getElementById('btn-gift-open');
 const popup = document.getElementById('gift-popup');
 const closeBtn = document.getElementById('gift-popup-close');
+const nameField = document.querySelector('#gift-popup input[name="usernamegift"]').parentNode;
+const emailField = document.querySelector('#gift-popup input[name="emailgift"]').parentNode;
+const inputGiftForm = document.querySelector('#inputs-giftform');
 
 function popupToggle() {
+
     popup.classList.toggle('hidden');
 };
 
-function initializeField()
+const ERROR_CLASS_NAME = "st-usernamegift-error";
+const FOCUSED_CLASS_NAME = "st-usernamegift-focused";
+
+function initializeField(field) {
+    const input = field.getElementsByTagName('input')[0];
+    const fieldError = field.querySelector('.st-usernamegift-error-msg');
+
+    input.value = '';
+    field.classList.remove(ERROR_CLASS_NAME);
+    field.classList.remove(FOCUSED_CLASS_NAME);
+    fieldError.innerText = '';
+
+    input.onfocus = () => {
+        field.classList.add(FOCUSED_CLASS_NAME);
+    };
+    input.onblur = () => {
+        if (!input.value) {
+            field.classList.remove(FOCUSED_CLASS_NAME);
+        }
+
+    };
+    return {
+        getValue() {
+            return input.value;
+        }
+    }
+};
+
+const nameFieldUtils = initializeField(nameField);
+const emailFieldUtils = initializeField(emailField);
 
 openBtn.onclick = popupToggle;
 closeBtn.onclick = popupToggle;
+
+const HadleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+        name: nameFieldUtils.getValue(),
+        email: emailFieldUtils.getValue(),
+    }
+
+    const url = new URL('http://inno-ijl.ru/multystub/stc-21-03/feedback');
+    url.search = new URLSearchParams(data).toString();
+
+
+    fetch(url.toString());
+};
+
+inputGiftForm.addEventListener('submit', HadleSubmit)
--- target/gift.js
+++ target/gift.js
@@ -3,11 +3,58 @@
 var openBtn = document.getElementById('btn-gift-open');
 var popup = document.getElementById('gift-popup');
 var closeBtn = document.getElementById('gift-popup-close');
+var nameField = document.querySelector('#gift-popup input[name="usernamegift"]').parentNode;
+var emailField = document.querySelector('#gift-popup input[name="emailgift"]').parentNode;
+var inputGiftForm = document.querySelector('#inputs-giftform');
 
 function popupToggle() {
   popup.classList.toggle('hidden');
 }
 
 ;
+var ERROR_CLASS_NAME = "st-usernamegift-error";
+var FOCUSED_CLASS_NAME = "st-usernamegift-focused";
+
+function initializeField(field) {
+  var input = field.getElementsByTagName('input')[0];
+  var fieldError = field.querySelector('.st-usernamegift-error-msg');
+  input.value = '';
+  field.classList.remove(ERROR_CLASS_NAME);
+  field.classList.remove(FOCUSED_CLASS_NAME);
+  fieldError.innerText = '';
+
+  input.onfocus = function () {
+    field.classList.add(FOCUSED_CLASS_NAME);
+  };
+
+  input.onblur = function () {
+    if (!input.value) {
+      field.classList.remove(FOCUSED_CLASS_NAME);
+    }
+  };
+
+  return {
+    getValue: function getValue() {
+      return input.value;
+    }
+  };
+}
+
+;
+var nameFieldUtils = initializeField(nameField);
+var emailFieldUtils = initializeField(emailField);
 openBtn.onclick = popupToggle;
 closeBtn.onclick = popupToggle;
+
+var HadleSubmit = function HadleSubmit(event) {
+  event.preventDefault();
+  var data = {
+    name: nameFieldUtils.getValue(),
+    email: emailFieldUtils.getValue()
+  };
+  var url = new URL('http://inno-ijl.ru/multystub/stc-21-03/feedback');
+  url.search = new URLSearchParams(data).toString();
+  fetch(url.toString());
+};
+
+inputGiftForm.addEventListener('submit', HadleSubmit);
